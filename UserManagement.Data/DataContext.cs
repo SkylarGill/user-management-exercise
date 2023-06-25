279fc96655fe0acec73ed12742711c019d874cce
Skylar Gill <skylargilldev@gmail.com>
2023-06-25 15:41:59 +0100

    Remove unused using statement

--- UserManagement.Data/DataContext.cs
+++ UserManagement.Data/DataContext.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Infrastructure;
 using UserManagement.Data.Entities;
 
 namespace UserManagement.Data;
